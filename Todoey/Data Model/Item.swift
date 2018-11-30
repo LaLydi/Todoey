35aa44969239f9c0a4c5c73d6b5e200dfab715d5
Lydi <lydiamariong@gmail.com>
2018-11-29 19:53:51 -0600

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lydia Marion on 11/29/18.
+//  Copyright © 2018 Lydia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
